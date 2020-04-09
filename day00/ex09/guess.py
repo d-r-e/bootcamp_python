8ca1c0f841a6e90886d25eb7700a36bf7e22ba03
David R <egoz3ntrum@gmail.com>
2020-04-09 16:41:17 +0200

    added guess.py

---
+++ day00/ex09/guess.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python3
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    guess.py                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: darodrig <darodrig@42madrid.com>           +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2020/04/09 16:22:11 by darodrig          #+#    #+#              #
+#    Updated: 2020/04/09 16:22:11 by darodrig         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+from random import randint
+
+if __name__ == "__main__":
+	nb = randint(1, 99)
+	attempts = 0
+	print("You have to enter a number between 1 and 99 to find out the",
+			"secret number.\nType 'exit' to end the game.\n")
+	while (1):
+		print("What's your guess between 1 and 99?")
+		x = input()
+		if x == "exit":
+			exit()
+		attempts += 1
+		if not x.isdigit():
+			print("That's not a number.")
+		elif int(x) == nb:
+			print("Congratulations, you've got it!")
+			print("You won in", attempts, "attempts!")
+			exit()
+		elif int(x) < nb:
+			print("Too low!")
+		else:
+			print("Too high!")
+
