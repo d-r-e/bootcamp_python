9af563bef2d419caac31cb6d06c4abd8c64462f7
David R <egoz3ntrum@gmail.com>
2020-04-13 09:57:22 +0200

    simpler version

--- day00/ex05/kata00.py
+++ day00/ex05/kata00.py
@@ -2,13 +2,8 @@
 
 def main():
 	t = (19, 42, 21)
-	print("The 3 numbers are: ", end='')
-	for each in t:
-		print(each, end='')
-		if each != t[len(t) - 1]:
-			print(', ', end='')
-		else:
-			print()
+	print("The 3 numbers are: "
+		+ (', '.join(str(each) for each in t)))
 
 if __name__ == "__main__":
 	main()
