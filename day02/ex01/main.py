8376eead2f7d4a2e53ba1bc85fbabffc68dcfc06
David R <egoz3ntrum@gmail.com>
2020-04-15 00:02:41 +0200

    args kwargs

---
+++ day02/ex01/main.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python3
+
+
+def what_are_the_vars(*args, **kwargs):
+    obj = ObjectC()
+    for i in range(len(args)):
+        setattr(obj, "var_{}".format(str(i)), args[i])
+    for key, value in kwargs.items():
+        if key in dir(obj):
+            return
+        setattr(obj, key, value)
+    return obj
+
+
+class ObjectC(object):
+
+    def __init__(self):
+        pass
+
+
+def doom_printer(obj):
+    if obj is None:
+        print("ERROR")
+        print("end")
+        return
+    for attr in dir(obj):
+        if attr[0] != '_':
+            value = getattr(obj, attr)
+            print("{}: {}".format(attr, value))
+    print("end")
+
+
+if __name__ == '__main__':
+    obj = what_are_the_vars(7)
+    doom_printer(obj)
+    obj = what_are_the_vars("ft_lol", "Hi")
+    doom_printer(obj)
+    obj = what_are_the_vars()
+    doom_printer(obj)
+    obj = what_are_the_vars(12, "Yes", [0, 0, 0], a=10, hello="world")
+    doom_printer(obj)
+    obj = what_are_the_vars(42, a=10, var_0="world")
+    doom_printer(obj)
