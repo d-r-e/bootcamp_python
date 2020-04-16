6423222b862d9e1af7ba9cde1cebb3301a08239d
David R <egoz3ntrum@gmail.com>
2020-04-16 12:50:18 +0200

    Numpy exercises

---
+++ day03/ex00/NumPyCreator.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+def from_list(lst, type=None):
+    return np.array(lst, type)
+
+
+def from_tuple(tpl, type=None):
+    return np.array(tpl, type)
+
+
+def from_iterable(itr, type=None):
+    return np.array([elem for elem in itr])
+
+
+def from_shape(shape, value, type=None):
+    return np.full(shape, value, type)
+
+
+if __name__ == "__main__":
+    print(from_list([1, 2, 3.9, 4, 5], int),)
+    print(from_tuple((1, 2, 3, 4, 5)))
+    print(from_iterable(map(lambda x: x * 2, range(4))))
+    print(from_shape((5, 5), 1.99, int))
