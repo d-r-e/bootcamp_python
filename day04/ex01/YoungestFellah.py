7b778d160f7d18e9609b73b96bd4cfc23fc65480
David R <egoz3ntrum@gmail.com>
2020-04-17 14:00:57 +0200

    pandas loc function

--- day04/ex01/YoungestFellah.py
+++ day04/ex01/YoungestFellah.py
@@ -4,8 +4,8 @@ from FileLoader import FileLoader
 import pandas as pd
 
 def youngestFellah(df, year):
-    female = df['Age'].loc[df['Sex'] == 'F'].loc[df['Year'] == year].min(0)
-    male = df['Age'].loc[df['Sex'] == 'M'].loc[df['Year'] == year].min(0)
+    female = df['Age'].loc[df['Sex'] == 'F'].loc[df['Year'] == year].min()
+    male = df['Age'].loc[df['Sex'] == 'M'].loc[df['Year'] == year].min()
     return {'f': female, 'm': male}
 if __name__ == "__main__":
     data = FileLoader.load("../resources/athlete_events.csv")
