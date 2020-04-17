37f9e36f19461e5af14a6f7f9e16fba9b325e3e2
David R <egoz3ntrum@gmail.com>
2020-04-17 17:19:47 +0200

    ex02

--- day04/ex02/ProportionBySport.py
+++ day04/ex02/ProportionBySport.py
@@ -5,8 +5,8 @@ import pandas as pd
 def proportionBySport(df, year, sport, sex):
     total = df.loc[df['Year'] == year].loc[df['Sex'] == sex]
     portion = df.loc[df['Year'] == year].loc[df['Sport'] == sport].loc[df['Sex'] == sex]
-    total = total['ID'].count()
-    portion = portion['ID'].count()
+    total = total['ID'].drop_duplicates(keep='first').count()
+    portion = portion['ID'].drop_duplicates(keep='first').count()
     return portion / total
 
 
